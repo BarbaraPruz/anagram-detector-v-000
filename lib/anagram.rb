89bd5ace64e8517c6f0e6cf79b5938cd6ceec602
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-03 19:20:46 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,13 @@
 class Anagram
   attr_access  :word
-  
+
   def initalize (word)
     @word = word
   end
 
   def match ( candidates )
+    word_sorted = @word.split('').sort
+    candidates.detect  { | candidate | candidate.split('').sort == word_sorted }
   end
 
 end
